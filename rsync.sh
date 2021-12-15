17aa76b74adb4bca2311f85c89c4d6a3c7c8573d
Eugene Yokota <eed3si9n@gmail.com>
2021-12-15 01:54:43 -0500

    work around rsync issue

--- rsync.sh
+++ rsync.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -e
 
-rsync -avz --exclude=.DS_Store public/ bitnami@eed3si9n.com:/home/bitnami/apps/portal/htdocs/
+rsync -e '/usr/bin/ssh' -avz --exclude=.DS_Store public/ bitnami@eed3si9n.com:/home/bitnami/apps/portal/htdocs/
